948ce7725ac7cb083bc32927709757f370da0278
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-22 14:26:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+array = [1, 2, 3]
+square_array(array).each do |number|
+  puts "array value #{number} is #{number ** 2} when squared." == new_numbers
+end 
+  return new_numbers
 end
